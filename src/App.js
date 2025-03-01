2a7017320d1f80bc214e7dc26cbac2962a39c6ff
dilleshdill <dilleshnakkina222@gmail.com>
2025-03-01 15:24:38 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -7,25 +7,40 @@ import NotFound from './Components/NotFound/NotFound';
 import Products from './Components/Products/Products';
 import Cart from './Components/Cart/Cart';
 import ProtectedRoutes from './Components/ProtectedRoutes/ProtectedRoutes';
-import ProductItemDetails from './Components/ProductItemDetails/ProductItemDetails';
+import { Component } from 'react';
 import ProductWraper from './Components/ProductItemDetails/ProductWraper';
-const App= () =>{
-  console.log(window.innerWidth)
-  return (
-    
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/header" element={<Header />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route path='*' element = {<NotFound />} />
-        <Route element={<ProtectedRoutes />}>
-          <Route exact path='/products' element={<Products />} />
-          <Route exact path='/cart' element={<Cart />} />
-          <Route exact path='/' element = {<Home />} />
-          <Route exact path='/products/:id' element = {<ProductWraper/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+import CartContext from './Context/CartContext';
+class App extends Component{
+  state = {cartList:[]}
+
+  addCartItem = (cartItem) => {
+    this.setState(prevState => ({
+      cartList: [...prevState.cartList,cartItem]
+    }))
+  }
+
+  deleteCartItem = (id) => {}
+  render(){
+    const {cartList} = this.state;
+    return (
+      
+      <BrowserRouter>
+      <CartContext.Provider value={{cartList,addCartItem:this.addCartItem,deleteCartItem:this.deleteCartItem}}>
+        <Routes>
+          <Route exact path="/header" element={<Header />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route path='*' element = {<NotFound />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route exact path='/products' element={<Products />} />
+            <Route exact path='/cart' element={<Cart />} />
+            <Route exact path='/' element = {<Home />} />
+            <Route exact path='/products/:id' element = {<ProductWraper/>} />
+          </Route>
+        </Routes>
+        </CartContext.Provider>
+      </BrowserRouter>
+    );
+  }
+  
 }
 export default App;
